03def86b3cfd0890fb243e5389e52d232791fd6d
Diana Draia1 <diana.draia1@student.upt.ro>
2024-04-03 19:47:45 +0300

    started working on a level
    i have a player that moves when I press space and when hits a wall returns

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] int speed;
+    float speedMultiplier;
+
+    [Range(1,10)]
+    [SerializeField] float acceleration;
+
+    bool btnPressed;
+
+    //detect when player hit the wall and change direction
+    bool isWallTouch;
+    public LayerMask wallLayer;
+    public Transform wallCheckPoint;
+
+    Vector2 relativeTransform;
+
+    public void Start()
+    {
+        UpdateRelativeTransform();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        updateSpeedMultiplier();
+        float targetSpeed = speed * speedMultiplier * relativeTransform.x;
+
+        rb.velocity = new Vector2(targetSpeed, rb.velocity.y);
+
+        isWallTouch = Physics2D.OverlapBox(wallCheckPoint.position, new Vector2(0.12f, 0.8f), 0, wallLayer);
+
+        if(isWallTouch)
+        {
+            Flip();
+        }
+    }
+
+    public void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        UpdateRelativeTransform();
+    }
+
+    public void UpdateRelativeTransform()
+    {
+        relativeTransform = transform.InverseTransformVector(Vector2.one);
+    }
+
+
+    public void Move(InputAction.CallbackContext value)
+    {
+        if(value.started)
+        { 
+            btnPressed = true;
+        }
+        else if(value.canceled) 
+        {
+            btnPressed= false;
+        }
+        
+    }
+
+    void updateSpeedMultiplier()
+    {
+        if (btnPressed && speedMultiplier < 1) 
+        {
+            speedMultiplier += Time.deltaTime * acceleration ;
+        }
+
+        if (!btnPressed && speedMultiplier > 0)
+        {
+            speedMultiplier -= Time.deltaTime * acceleration;
+            if (speedMultiplier < 0) speedMultiplier = 0;
+        }
+    }
+}
