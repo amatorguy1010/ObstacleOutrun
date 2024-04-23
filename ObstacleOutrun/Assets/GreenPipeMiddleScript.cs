7ecfd16662583209f4f353ba043acd73cbc11213
Alexandru Campan <alexandrucampan18@gmail.com>
2024-04-23 14:36:12 +0300

    Fix score for level 2

--- ObstacleOutrun/Assets/GreenPipeMiddleScript.cs
+++ ObstacleOutrun/Assets/GreenPipeMiddleScript.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class PipeMiddleScript : MonoBehaviour
+public class GreenPipeMiddleScript : MonoBehaviour
 {
-
     public GreenLogicScript logic;
     // Start is called before the first frame update
     void Start()
     {
-        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<GreenLogicScript>();
+        logic = GameObject.FindGameObjectWithTag("LogicGreen").GetComponent<GreenLogicScript>();
     }
 
     // Update is called once per frame
@@ -20,7 +18,6 @@ public class PipeMiddleScript : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Colide");
         logic.addScore();
     }
 }
--- ObstacleOutrun/Assets/SuperBirdScript.cs
+++ ObstacleOutrun/Assets/SuperBirdScript.cs
@@ -7,19 +7,27 @@ public class SuperBirdScript : MonoBehaviour
 
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public GreenLogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("LogicGreen").GetComponent<GreenLogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true) {
+        if(Input.GetKeyDown(KeyCode.Space) == true  && birdIsAlive) {
 
             myRigidBody.velocity = Vector2.up * flapStrength;
         }
        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.GameOver();
+        birdIsAlive = false;
     }
 }
