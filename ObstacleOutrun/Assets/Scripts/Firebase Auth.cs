b7fd29c97a6b58adb7f869272c77a1b85fad7bfc
Alexandru Campan <alexandrucampan18@gmail.com>
2024-05-08 21:43:21 +0300

    Login System

--- ObstacleOutrun/Assets/Scripts/Firebase Auth.cs
+++ ObstacleOutrun/Assets/Scripts/Firebase Auth.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 
 public class FirebaseAuth : MonoBehaviour
 {
-    public GameObject loginPanel, signupPanel, profilePanel, forgetPasswordPanel;
+    public GameObject loginPanel, signupPanel, profilePanel, forgetPasswordPanel,notificationPanel;
 
     public TMP_InputField loginEmail, loginPassword, signupEmail, signupPassword, signupCPassword, signupUserName, forgetPassEmail;
+
+    public Text notif_Title_Text, notif_Message_Text,profileUserName_Text,profileUserEmail_Text;
+
+    public Toggle rememberMe;
 
     public void OpenLoginPanel()
     {
@@ -51,6 +55,7 @@ public class FirebaseAuth : MonoBehaviour
     {
         if(string.IsNullOrEmpty(loginEmail.text) && string.IsNullOrEmpty(loginPassword.text))
         {
+            showNotificationMessage("Error", "Fields Are Empty");
             return;
         }
 
@@ -61,6 +66,7 @@ public class FirebaseAuth : MonoBehaviour
     {
         if(string.IsNullOrEmpty(signupEmail.text) &&  string.IsNullOrEmpty(signupPassword.text) && string.IsNullOrEmpty(signupCPassword.text) && string.IsNullOrEmpty(signupUserName.text))
         {
+            showNotificationMessage("Error", "Fields Are Empty");
             return;
         }
 
@@ -70,8 +76,31 @@ public class FirebaseAuth : MonoBehaviour
     public void forgetPass()
     {
         if (string.IsNullOrEmpty(forgetPassEmail.text))
-        { 
+        {
+            showNotificationMessage("Error", "Forget Email Empty");
             return;
         }
+    }
+
+    private void showNotificationMessage(string title, string message)
+    {
+        notif_Title_Text.text ="" + title;
+        notif_Message_Text.text = "" + message;
+        notificationPanel.SetActive(true);
+    }
+
+    public void CloseNotif_Panel()
+    {
+        notif_Title_Text.text = "" ;
+        notif_Message_Text.text = "";
+        notificationPanel.SetActive(false);
+    }
+
+    public void LogOut()
+    {
+        profilePanel.SetActive(false);
+        profileUserEmail_Text.text = "";
+        profileUserName_Text.text = "";
+        OpenLoginPanel();
     }
 }
